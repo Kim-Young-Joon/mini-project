c2717627dbce23a6ad9fd40240078c12964b75ba
JJiWoNing <jjiwoning12@gmail.com>
2022-08-19 21:38:40 +0900

    #3_Entity_Table 엔티티 설계 V1, Comment Entity 추가 및 일부 수정

--- graduate-project/src/main/java/com/soongsil/graduateproject/repository/MemberRepository.java
+++ graduate-project/src/main/java/com/soongsil/graduateproject/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.soongsil.graduateproject.repository;
 
+import com.soongsil.graduateproject.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,14 @@ import javax.persistence.EntityManager;
 public class MemberRepository {
 
     private final EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Long update(Member member){
+        //To Do List
+        return 1L;
+    }
 }
