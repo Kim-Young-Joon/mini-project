c28b67a09b23e7c82c4cc64cf2d2c16ba4794ace
youngjoon <yj3723@naver.com>
2022-10-19 23:07:31 +0900

    게시판 목록 및 댓글 부분 레이아웃

--- graduate-project/src/main/resources/static/js/comment.js
+++ graduate-project/src/main/resources/static/js/comment.js
@@ -54,16 +54,12 @@ function getCommentList(myUrl){
 
             if(data.length > 0){
                 for(i=0; i<data.length; i++){
-                    html += "<div>";
-                    html += "<div><table class='table'><tr><td><h6><strong>"+data[i].memberName+"</strong></h6></td>";
+                    // html += "<div>";
+                    html += "<tr><td><strong>"+data[i].memberName+"</strong></td>";
                     html += "<td>" + data[i].content + "</td></tr>";
-                    html += "</table></div>";
-                    html += "</div>";
                 }
             } else {
-                html += "<div>";
-                html += "<div><table class='table'><h6><strong>등록된 댓글이 없습니다.</strong></h6>";
-                html += "</table></div>";
+                html += "<div><h6><strong>등록된 댓글이 없습니다.</strong></h6>";
                 html += "</div>";
             }
 
