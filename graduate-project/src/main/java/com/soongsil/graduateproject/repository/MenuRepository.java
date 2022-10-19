92105efd942e3eb91ecfaf311812a96135ff3aa2
JJiWoNing <jjiwoning12@gmail.com>
2022-10-19 21:55:39 +0900

    Repository 추가

---
+++ graduate-project/src/main/java/com/soongsil/graduateproject/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.soongsil.graduateproject.repository;
+
+import com.soongsil.graduateproject.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
